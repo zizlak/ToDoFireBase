e9cabf30a71926c814e4a555c5b25dc913ed7954
Aleksandr Kurdiukov <runoleonard@gmail.com>
2020-05-25 17:38:11 +0200

    Initial Commit

---
+++ ToDo.FireBase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo.FireBase
+//
+//  Created by Aleksandr Kurdiukov on 25.05.20.
+//  Copyright © 2020 Zizlak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
